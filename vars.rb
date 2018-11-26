983223dba6a50ba856e2c8ccc472c3d407e7f61f
Spike Burton <ralphburton3@gmail.com>
2018-11-26 16:11:04 +0000

    automatically backed up by learn

---
+++ vars.rb
@@ -0,0 +1,10 @@
+first_number = 7
+second_number = 14
+
+sum = first_number + second_number
+
+puts sum
+
+current_president = "Barack Obama"
+
+puts "In 2016, the president was #{current_president}."
